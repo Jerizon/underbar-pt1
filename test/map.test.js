7b6db4708b7809b68c72f76c54b7006d2a26fa17
Jerizon <JesseLowellReyes@gmail.com>
2017-05-05 16:57:31 -0700

    "Add test to underbar function map"

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,11 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  //Jey's Test
+  it('creates a new array and does not change the original array', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(arr, (el) => el * 23);
+    expect(arr).toEqual([1, 2, 3, 4, 5]);
+  });
 });
