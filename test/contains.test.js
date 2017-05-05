91d459208edf9bddc50065b3fef6737bc5765d7b
Jerizon <JesseLowellReyes@gmail.com>
2017-05-05 16:59:01 -0700

    "Add test to underbar function contains"

--- test/contains.test.js
+++ test/contains.test.js
@@ -31,4 +31,16 @@ describe('contains()', () => {
     expect(_.contains(ponies, 'Applejack')).toBe(false);
   });
 
+  //Jey's Tests
+  it('returns true if an array contains multiples of the target', () => {
+    const nums = [1, 4, 4, 4, 5];
+    expect(_.contains(nums, 4)).toBe(true);
+  });
+  
+  it('returns false if an array contains abstract-comparison-truth target', () => {
+    expect(1 == true);
+    const nums = [true, 2, 3, 4, 5];
+    expect(_.contains(nums, 1)).toBe(false);
+  });
+
 });
