8c71eb477400c2bd894d99394bbe544c4f93df99
Jerizon <JesseLowellReyes@gmail.com>
2017-05-05 17:00:10 -0700

    "Add test to underbar function some"

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,24 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  //Jey's tests
+  it('returns true if at least one number in an object are odd', () => {
+      const nums = {
+        a: 2,
+        b: 4,
+        c: 6,
+        d: 7
+      };
+      expect(_.some(nums, num => num % 2 === 1)).toBe(true);
+    });
+
+    it('returns false if no numbers in an object are odd', () => {
+      const nums = {
+        a: 2,
+        b: 4,
+        c: 6,
+        d: 8
+      };
+      expect(_.some(nums, num => num % 2 === 1)).toBe(false);
+    });
 });
